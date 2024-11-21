6be0e55b7ec8681ab3b1581773c0ed6cd8f472e1
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-21 17:55:37 +0700

    menampilkan halaman form untuk memasukkan data mahasiswa dan mengarah ke halaman Data

--- app/src/main/java/com/example/navigationn/Navigasi.kt
+++ app/src/main/java/com/example/navigationn/Navigasi.kt
@@ -6,10 +6,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.navigationn.model.ListGender
+import com.example.navigationn.ui.view.FormMahasiswaView
 import com.example.navigationn.ui.viewmodel.Mahasiswaviewmodel
 
 enum class Halaman{
@@ -29,7 +33,19 @@ fun Navigasi(
             modifier = modifier.padding(isipadding),
             navController = navHost, startDestination = Halaman.Form.name
         ) {
-
+            composable(route = Halaman.Form.name) {
+                val konteks = LocalContext.current
+                FormMahasiswaView(
+                    //Di bawah ini merupakan dari parameter halaman FormulirView
+                    listGender = ListGender.ListGender.map { id ->
+                        konteks.getString(id)
+                    },
+                    onSubmitClick = {
+                        viewModel.saveDataMhs(it)
+                        navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
         }
     }
 }
