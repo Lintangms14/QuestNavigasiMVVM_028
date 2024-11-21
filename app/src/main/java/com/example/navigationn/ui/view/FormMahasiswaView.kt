3970f8460f6b73baf8539de4685f190465acb346
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-21 17:35:04 +0700

    menampilkan judul dan kolom input untuk nama

--- app/src/main/java/com/example/navigationn/ui/view/FormMahasiswaView.kt
+++ app/src/main/java/com/example/navigationn/ui/view/FormMahasiswaView.kt
@@ -1,11 +1,22 @@
 package com.example.navigationn.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun FormMahasiswaView(
@@ -22,4 +33,22 @@ fun FormMahasiswaView(
     val listData: MutableList<String> = mutableListOf(
         nama, memilihJK, alamat
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Biodata", fontSize = 16.sp, fontWeight = FontWeight.Bold)
+
+        Spacer(Modifier.padding(16.dp))
+        TextField(
+            modifier = modifier.fillMaxWidth().padding(5.dp),
+            value = nama    ,
+            onValueChange = { nama = it },
+            label = { Text("nama") },
+            placeholder = { Text("Masukkan Nama Anda") }
+        )
+    }
 }
