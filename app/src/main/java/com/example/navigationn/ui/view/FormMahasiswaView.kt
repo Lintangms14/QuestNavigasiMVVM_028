cda54f837a6aba7cde1a740198d244d64191224b
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-21 17:36:14 +0700

    untuk menampilkan pilihan jenis kelamin menggunakan RadioButton

--- app/src/main/java/com/example/navigationn/ui/view/FormMahasiswaView.kt
+++ app/src/main/java/com/example/navigationn/ui/view/FormMahasiswaView.kt
@@ -1,10 +1,12 @@
 package com.example.navigationn.ui.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -50,5 +52,22 @@ fun FormMahasiswaView(
             label = { Text("nama") },
             placeholder = { Text("Masukkan Nama Anda") }
         )
+
+        Row {
+            listGender.forEach { item ->
+                Row (
+                    verticalAlignment = Alignment.CenterVertically
+                ){
+                    RadioButton(
+                        selected = memilihJK == item,
+                        onClick = {
+                            memilihJK = item
+                        }
+                    )
+
+                    Text(item)
+                }
+            }
+        }
     }
 }
