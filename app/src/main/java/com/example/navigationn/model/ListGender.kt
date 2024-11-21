85845c60ee743672a32f33a507c7f9d7f41b0682
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-21 17:13:53 +0700

    untuk menyimpan referensi ke sumber daya string untuk jenis kelamin

---
+++ app/src/main/java/com/example/navigationn/model/ListGender.kt
@@ -0,0 +1,10 @@
+package com.example.navigationn.model
+
+import com.example.navigationn.R
+
+object ListGender {
+    val ListGender = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
