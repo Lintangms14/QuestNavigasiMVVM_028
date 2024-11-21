b1f7b08910889bbc4cb4b253486fa49f247856c5
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-21 17:21:17 +0700

    di gunakan untuk menampilkan detail mahasiswa

---
+++ app/src/main/java/com/example/navigationn/ui/view/DetailMahasiswaView.kt
@@ -0,0 +1,27 @@
+package com.example.navigationn.ui.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.navigationn.model.Mahasiswa
+
+@Composable
+fun DetailMahasiswaView(
+    dataMhs: Mahasiswa,
+    modifier: Modifier,
+    onClickButton: () -> Boolean
+){
+    val listDataMhs = listOf(
+        Pair("Nama", dataMhs.nama),
+        Pair("Gender", dataMhs.gender),
+        Pair("Alamat", dataMhs.alamat)
+    )
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {}
+}
