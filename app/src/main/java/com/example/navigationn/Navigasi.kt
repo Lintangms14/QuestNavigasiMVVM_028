723f40921120d13b15565c14aed5c617dfa66101
Lintangms14 <merayusukmalintang@gmail.com>
2024-11-21 17:54:07 +0700

    mengatur tampilan aplikasi dengan struktur dasar dan navigasi antar halaman

--- app/src/main/java/com/example/navigationn/Navigasi.kt
+++ app/src/main/java/com/example/navigationn/Navigasi.kt
@@ -1,6 +1,35 @@
 package com.example.navigationn
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationn.ui.viewmodel.Mahasiswaviewmodel
+
 enum class Halaman{
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: Mahasiswaviewmodel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+){
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+        NavHost(
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestination = Halaman.Form.name
+        ) {
+
+        }
+    }
 }
